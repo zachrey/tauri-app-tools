c7fca3d8708584ecc70706dbe94a6b2b61533ea9
Zhe Zhang <zz__0123@163.com>
2023-07-28 17:57:33 +0800

    feat: 引入 antd 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { UpCircleTwoTone } from '@ant-design/icons';
 import "./App.css";
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to Tauri!</h1>
-
+      <UpCircleTwoTone />
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo vite" alt="Vite logo" />
